d44e9738c71dddbfc639542b5c92f08829a6e5b8
xNauty <52219849+xNauty@users.noreply.github.com>
2022-06-23 14:10:38 +0200

    Commit nr 1

---
+++ Drugi.py
@@ -0,0 +1,9 @@
+print("Drugi plik")
+
+napis = "WITAM"
+
+for x in napis:
+    print(x)
+
+input("Wcisnij Enter by zamknac okno")
+    
---
+++ Pierwszy.py
@@ -0,0 +1,7 @@
+print("Pierwszy plik")
+kogos_nr = input("Podaj ulubiony numer: ")
+moj_nr = 27
+
+print("Myle że liczba", kogos_nr, "jest ciekawym szczesliwym numerem, natomiast mój to",  moj_nr, " :)")
+
+input("Wcisnij Enter by zamknac okno")
