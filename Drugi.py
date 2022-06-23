195f15a4ee7b1de80e1862fc615d2ed38c39ec15
xNauty <52219849+xNauty@users.noreply.github.com>
2022-06-23 14:13:16 +0200

    Commit nr 2

--- Drugi.py
+++ Drugi.py
@@ -5,5 +5,7 @@ napis = "WITAM"
 for x in napis:
     print(x)
 
+print("Commit nr 2")
+
 input("Wcisnij Enter by zamknac okno")
     
--- Pierwszy.py
+++ Pierwszy.py
@@ -4,4 +4,6 @@ moj_nr = 27
 
 print("Myle że liczba", kogos_nr, "jest ciekawym szczesliwym numerem, natomiast mój to",  moj_nr, " :)")
 
+print("commituuuje 2 raz")
+
 input("Wcisnij Enter by zamknac okno")
